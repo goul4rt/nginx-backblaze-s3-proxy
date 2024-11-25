d849aa0ba08def1f119a2afa418f2dcf4d59ae29
aroldogooulart@estuda.com <=>
2024-11-25 10:15:06 -0400

    a

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@
 envsubst '$B2_KEY_ID $B2_ACCESS_KEY $B2_BUCKET $B2_REGION $B2_AUTH_HEADER $ALLOWED_REFERER $ALLOWED_HEADER $CACHE_RAM $CACHE_DISK $CACHE_INACTIVITY $CACHE_MAX_AGE $DEBUG_UUID $MAX_CONNECTIONS_PER_IP $ALLOWED_HOST_REGEX' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
 
 # Inicia o Fail2Ban
-service fail2ban start
+#service fail2ban start
 
 # Inicia o Nginx
 exec nginx -g 'daemon off;'
